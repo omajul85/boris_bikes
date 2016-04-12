cca4cce92bd8a79ec21b6d2bd5638e00f0359278
Lexi Tran <hazukitran@gmail.com>
2016-04-12 17:10:36 +0700

    Step 10

--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -1,7 +1,10 @@
 require 'Bike'
 
-describe Bike do
-    
-    it { is_expected.to respond_to(:working?) }
-    
+describe Bike do 
+  it { is_expected.to respond_to(:working?) }
+
+  it 'responds to working?' do
+    expect(subject).to be_working
+  end
+
 end
